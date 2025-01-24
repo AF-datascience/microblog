6fb502758639b4d32ffeed1e4403b132485eab09
AF-datascience <afazil1994@gmail.com>
2025-01-24 16:02:44 +0000

    making database for app

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
 # import flask class
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy 
 from forms import RegistrationForm, LoginForm
+from datetime import datetime
 
 # app is an instance of flask viarable 
 # __name__ is the name of the module, this is the same as main - so flask knows where to look
@@ -9,6 +11,55 @@ app = Flask(__name__)
 
 # set a secret key to stop people messing with the app
 app.config['SECRET_KEY'] = 'fea587cc66094ff6e53b8b3288d38d0d'
+# set a location for the sql database: 
+# relative path from the current file
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+
+# create database instance 
+db = SQLAlchemy(app)
+
+
+# class models are the database 
+# each class is a table in the database 
+
+# class or model for the users 
+class Users(db.Model): 
+    # columns for table: 
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique = True, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    image_file = db.Column(db.string(20), nullable = False, default = 'default.jpg')
+    password = db.Column(db.string(60), nullable = False)
+
+    # the post model has a relationship to teh user model 
+    # beckref lets u find the user who made the post 
+    # lazy loads data in one go 
+    posts = db.relationship('Post', backref = 'author', lazy = True)
+    # this references hte post class
+    # a repr method 
+    # how an object is printed
+
+    def __repr__(self): 
+        return f"User {self.username}, email {self.email}. {self.image}"
+
+# so a post can belong to one user only 
+# but a user can have many posts 
+# this is a one to many relationship, one author but many posts 
+# table names iwll be se to lowercase e.g. User class makes table user. 
+# specify the user in the posts model 
+
+# class to model the Posts: 
+class Post(db.Model): 
+    id = db.Column(db.integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)   
+    data_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    content = db.Column(db.Text, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    # here the reference is the table name and column name 
+    # creating attribute above forthe user int he posts moel
+
+    def __repr__(self): 
+        return f"Post '{self.title} and date: {self.date_posted}"
 
 # dummy data: 
 # can be used int he template for home.html using jinja 
